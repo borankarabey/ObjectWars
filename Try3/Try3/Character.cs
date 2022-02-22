9a92630000882cb50146110ecefc4eedf3a49efc
Emre <73621311+borankarabey@users.noreply.github.com>
2022-02-22 19:59:00 +0300

    Add files via upload

---
+++ Try3/Try3/Character.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Character
+{
+	
+	public enum Hobbits
+    {
+
+    }
+	
+	public enum Elves 
+    {
+
+    }
+
+	public enum Humans
+    {
+
+    }
+
+	public enum Midgets
+    {
+
+    }
+
+	public enum Orgs
+    {
+
+    }
+
+    public enum Goblins
+    {
+
+    }
+
+    public enum BlackGhosts
+    {
+
+    }
+
+    public enum Gandalf
+    {
+
+    }
+
+    public enum Saruman
+    {
+
+    }
+
+    public enum Groot
+    {
+
+    }
+
+    public enum Dragon
+    {
+
+    }
+
+    
+
+}
